d28a59f40a5b4efa6cb8d37d4e577838cb3a806f
Asim Aslam <asim@chuhnk.me>
2018-07-07 13:13:31 +0100

    Setup app

--- cmd/micro/main.go
+++ cmd/micro/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"github.com/micro/micro/cmd"
+	"github.com/micro/go-micro/cmd"
+	dmc "github.com/micro/micro/cmd"
+)
+
+var (
+        name        = "micro"
+        description = "An enterprise cloud-native toolkit"
+        version     = "0.1.0"
 )
 
 func main() {
-	cmd.Init()
+	// setup the command line
+	dmc.Setup(cmd.App())
+
+	// initialise command line
+	cmd.Init(
+                cmd.Name(name),
+                cmd.Description(description),
+                cmd.Version(version),
+	)
 }
