6ca3d03e39d477827ca1e2789e5167f3f2386c5d
Asim Aslam <asim@aslam.me>
2019-01-13 10:09:25 +0000

    Set default router

--- go/proxy/http/options.go
+++ go/proxy/http/options.go
@@ -10,6 +10,13 @@ func WithBackend(url string) micro.Option {
 	return func(o *micro.Options) {
 		// get the router
 		r := o.Server.Options().Router
+
+		// not set
+		if r == nil {
+			r = DefaultRouter
+			o.Server.Init(server.WithRouter(r))
+		}
+
 		// check its a http router
 		if httpRouter, ok := r.(*Router); ok {
 			httpRouter.Backend = url
